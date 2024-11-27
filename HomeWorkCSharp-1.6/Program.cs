36f66b1a92a4d70ddc392242ccc17a9e81403705
Владислав Васин <VasinVladislav94@gmail.com>
2024-11-27 12:20:42 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkCSharp-1.6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCSharp_1._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.6.Вывести на одной строке числа 7, 15 и 100 с двумя пробелами между ними.
+            Console.WriteLine("5 15 100");
+        }
+    }
+}
